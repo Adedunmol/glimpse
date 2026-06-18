880765eb82dadf50682a467628a5a471caf7213d
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2026-06-18 12:19:50 +0100

    remove dead code

--- backend/internal/lib/aws/aws.go
+++ backend/internal/lib/aws/aws.go
@@ -25,20 +25,6 @@ func NewAWS(server *server.Server) (*AWS, error) {
 		)),
 	}
 
-	// Add custom endpoint if provided (for S3-compatible services like Sevalla)
-	// if awsConfig.EndpointURL != "" {
-	// 	configOptions = append(configOptions, config.WithEndpointResolverWithOptions(
-	// 		aws.EndpointResolverWithOptionsFunc(func(service, region string,
-	// 			options ...interface{},
-	// 		) (aws.Endpoint, error) {
-	// 			return aws.Endpoint{
-	// 				URL:           awsConfig.EndpointURL,
-	// 				SigningRegion: awsConfig.Region,
-	// 			}, nil
-	// 		}),
-	// 	))
-	// }
-
 	cfg, err := config.LoadDefaultConfig(context.TODO(), configOptions...)
 	if err != nil {
 		return nil, err
